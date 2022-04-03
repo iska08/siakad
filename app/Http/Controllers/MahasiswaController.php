5b2275fccb8f9909b05d37ad20b69b8076e6afa6
iska08 <aamuhaimin08@gmail.com>
2022-04-03 12:47:34 +0700

    Tugas Praktikum

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -41,7 +41,7 @@ class MahasiswaController extends Controller {
     public function search(Request $request)
     {
         $keyword = $request->search;
-        $mahasiswa = Mahasiswa::where('nama', 'like', "%" . $keyword . "%")->paginate(6);
+        $mahasiswa = Mahasiswa::where('nama', 'like', "%" . $keyword . "%")->paginate(3);
         return view('mahasiswa.index', compact('mahasiswa'))->with('i', (request()->input('page', 1) - 1) * 5);
     }
     public function show($Nim) {
